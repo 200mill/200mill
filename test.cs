eb1f0cd4422b97afb710c16af83291d6636410f9
200mill <97425242+200mill@users.noreply.github.com>
2023-07-12 02:11:50 +0000

    뀨 변경되었어<3

---
+++ test.cs
@@ -0,0 +1,20 @@
+// 네임스페이스를 만들어줘
+namespace test
+{
+    // 클래스를 만들어줘
+    class Program
+    {
+        // 메인함수를 만들어줘
+        static void Main(string[] args)
+        {
+            // 콘솔에 출력해줘
+            Console.WriteLine("Hello World!");
+            struct mathmathics
+            {
+                //double로 a, b, result 변수를 선언해줘
+                double a, b, result;
+            }
+        }
+    }
+}
+```
